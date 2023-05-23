835829596011076c03ea8ff162946306cf3b4152
KingKai69 <kai.horlacher@gmx.de>
2023-05-23 14:37:58 +0200

    Update

--- Streamlit_classification.py
+++ Streamlit_classification.py
@@ -1,48 +1,25 @@
 import streamlit as st
 from streamlit_shap import st_shap
 import shap
-from sklearn.model_selection import train_test_split
 import xgboost
 import numpy as np
 import pandas as pd
-np.bool = np.bool_
+import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
-import shap
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder, MinMaxScaler, StandardScaler, KBinsDiscretizer
-from sklearn.compose import ColumnTransformer
-from sklearn.impute import SimpleImputer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score, balanced_accuracy_score, f1_score, matthews_corrcoef, precision_score, recall_score, make_scorer
 from sklearn.model_selection import KFold, train_test_split, cross_val_score, cross_validate
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier, GradientBoostingClassifier, AdaBoostClassifier
-from sklearn.svm import SVC
-from sklearn.naive_bayes import GaussianNB
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.neural_network import MLPClassifier
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import KernelPCA
-from sklearn.model_selection import KFold, train_test_split, cross_val_score, cross_validate
+from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.model_selection import TimeSeriesSplit, cross_validate
-from imblearn.over_sampling import RandomOverSampler, SMOTE
-from imblearn.under_sampling import RandomUnderSampler
-import matplotlib.pyplot as plt
-from sklearn.feature_selection import SelectFromModel
-from imblearn.pipeline import Pipeline
-from sklearn.model_selection import RandomizedSearchCV
 from sklearn.metrics import ConfusionMatrixDisplay
-from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
-from sklearn.svm import SVR
-from sklearn.neural_network import MLPRegressor
-from sklearn.metrics import mean_squared_error, mean_absolute_error
-from sklearn.model_selection import train_test_split
-import matplotlib
-matplotlib.use('Agg')
+from sklearn.metrics import confusion_matrix
+
+#matplotlib.use('Agg')
+np.bool = np.bool_
 
 #######################################################
 ################ MACHINE LEARNING #####################
@@ -87,11 +64,6 @@ df_enc_3['symboling'] = df_enc_3['symboling'].replace(0, 'medium-risk')
 df_enc_3['symboling'] = df_enc_3['symboling'].replace(-1, 'low-risk')
 df_enc_3['symboling'] = df_enc_3['symboling'].replace(-2, 'low-risk')
 
-###### Modelling ######
-
-# Fixing the SEED
-SEED = 27
-
 # Define features 
 features = [
     'normalized-losses',
@@ -121,6 +93,19 @@ features = [
     'price',
 ]
 
+# Correlation analysis
+Val = df_enc_3[features]
+corrmat = Val.corr()
+#f, ax = plt.subplots(figsize=(12, 8))
+#corr_matrix = sns.heatmap(corrmat, vmax=.8, square=True)
+
+# Create dataframe with features with hight correlation
+corrmat_masked = corrmat.mask(abs(corrmat) >= 1)
+high_corr = corrmat_masked[(corrmat_masked > 0.6) | (corrmat_masked < -0.6)].stack().reset_index()
+high_corr.columns = ['Feature_A', 'Feature_B', 'Correlation']
+high_corr = high_corr[high_corr['Feature_A'] < high_corr['Feature_B']]
+high_corr.head(n=120)
+
 # Define features nonCor
 features_nonCor = [
     'make',
@@ -144,6 +129,17 @@ features_nonCor = [
     'price',
 ]
 
+# Df feature
+col_names = list(df.columns)
+df_feature = pd.DataFrame({'feature': col_names,
+                           'is_feature': [True if value in features_nonCor else False for value in col_names],
+                           'is_target_feature': [True if value == 'symboling' else False for value in col_names]})
+
+###### Modelling ######
+
+# Fixing the SEED
+SEED = 27
+
 # Define X and Y Data
 x_data = df_enc_3[features_nonCor]
 y_data = df_enc_3['symboling']
@@ -157,42 +153,62 @@ X_train, X_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.
 clf_final.fit(X_train, y_train)
 y_pred = clf_final.predict(X_test)
 
-##### Compute SHAP values #####
-explainer = shap.Explainer(clf_final, X_train)
-shap_values = explainer(X_train)
+cm = confusion_matrix(y_test, y_pred)
+sns.heatmap(cm, annot=True, cmap='Blues', fmt='d', xticklabels=clf_final.classes_, yticklabels=clf_final.classes_)
 
-explainer_tree = shap.TreeExplainer(clf_final)
-shap_values_tree = explainer.shap_values(X_train)
+df_pred_res = pd.DataFrame({'Test Label': y_test, 'Prediction Label': y_pred})
+def_pred_res_fil = df_pred_res[df_pred_res['Test Label'] != df_pred_res['Prediction Label']]
+def_pred_res_fil = def_pred_res_fil.sort_values('Test Label')
+
+
+##### Compute SHAP values #####
+#explainer = shap.Explainer(clf_final, X_train)
+#shap_values = explainer(X_train)
+
+explainer_tree = shap.TreeExplainer(clf_final, X_train)
+shap_values_tree = explainer_tree.shap_values(X_test)
 
 #######################################################
 ################ Explainable AI #######################
 #######################################################
 
 #import streamlit.components.v1 as components
-shap.initjs()
-
-#def st_shap(plot, height=None):
-#    shap_html = f"<head>{shap.getjs()}</head><body>{plot.html()}</body>"
-#    components.html(shap_html, height=height)
+#shap.initjs()
 
 # Some Basic settings
-st.set_option('deprecation.showPyplotGlobalUse', False)
+#st.set_option('deprecation.showPyplotGlobalUse', False)
 # Set dashboard width to entire width of screen
 st.set_page_config(layout="wide")
 
+
+#### title ####
 with st.container():
    st.title('Explainable AI with automobile dataset')
-
 st.divider()
 
-tab1, tab2, tab3 = st.tabs(["Dataframe", "Summary", "XAI"])
+tab1, tab2, tab3 = st.tabs(["Classification model", "XAI Summary", "XAI Detail"])
 
+#### Classification model ####
 with tab1:
-   st.write('Below is a snapshot of the original dataframe') 
+   st.write('Below is a snapshot of the original dataframe without any preprocessing steps:') 
    st.write(df.head(5))
    st.divider()
-   st.write('Below is a snapshot of the dataframe, with labeled encoded columns')
+   st.write('Below is a snapshot of the dataframe, with removed NaN values, labeled encoded columns and scaled features')
    st.write(df_imp.head(5))
+   st.divider()
+   st.subheader('Correlation Analysis')
+   st.write('After some preprocessing steps a correlations analysis was performed to identify pairs of features that have a high correlation. The goal is to remove features so that in the end no features have a high correlation. ')
+   
+   fig1, ax1 = plt.subplots(figsize=(4, 4))
+   sns.heatmap(corrmat, vmax=.8, square=True, ax=ax1)
+   st.pyplot(fig1)
+
+   st.divider()
+   st.write('Below all feature pairs with a correlation above 0.6 / below -0.6 are displayed. Accordingly for each feature pair, one feature is removed.')
+   st.dataframe(high_corr)
+   st.divider()
+   st.write('In the end the following features are used for modelling')
+   st.experimental_data_editor(df_feature)
    #st.table(x_data.columns)
    st.write('The machine learning model was trained with the following features to predict the target variable')
    for feature in features:
@@ -200,41 +216,49 @@ with tab1:
    st.write('Prediction target:')
    st.markdown(f"- Highway-mpg")
    st.divider()
+   st.title('Performance')
+   st.metric(label="F1-score", value="89,2 %", delta="XX")
+   fig, ax = plt.subplots()
+   sns.heatmap(cm, annot=True, cmap='Blues', fmt='d', xticklabels=clf_final.classes_, yticklabels=clf_final.classes_)
+   plt.figure(figsize=(4, 4))
+   plt.xlabel('Predicted')
+   plt.ylabel('Actual')
+   st.pyplot(fig)
 
-
+#### tab2 ####
 with tab2:
+   st.title('Shap Force Plot')
+   st.write('Explanation')
+   st_shap(shap.force_plot(explainer_tree.expected_value[0], shap_values_tree[0], X_test))
+   
+   st.title('Shap Summary Plot')
+   st.write('Explanation')
+   st_shap(shap.summary_plot(shap_values_tree, X_train, plot_type="bar"), height=400, width=1000) 
+    
+#### tab3 ####
+with tab3:
+    st.subheader('Overview false predictions')
+    st.dataframe(def_pred_res_fil)
 
-#Shap values 
-   shap.initjs()
 
-   st.write('Below is a snapshot of the Shap Force Plot')
-   #st_shap(shap.force_plot(explainer_tree.expected_value[0], shap_values_tree[0], X_test))
-   #st_shap(force_plot = shap.force_plot(explainer_tree.expected_value, shap_values_tree, X_test))
-   
-   st.write('Below is a snapshot of the Bar Chart for Mean Importance')
-   st_shap(shap.summary_plot(shap_values_tree, X_train, plot_type="bar")) 
-   
-   
-   #st_shap(shap.force_plot(explainer_tree.expected_value, shap_values_tree[0,:], X_train.iloc[0,:]), height=200, width=1000)
+
+
+
+ #st_shap(shap.force_plot(explainer_tree.expected_value, shap_values_tree[0,:], X_train.iloc[0,:]), height=200, width=1000)
    #st_shap(shap.force_plot(explainer_tree.expected_value, shap_values_tree, X_train), height=400, width=1000)
    #st_shap(shap.force_plot(explainer_tree.expected_value, shap_values_tree[0], X_train.iloc[0]), height=200, width=1000)
    #st_shap(shap.force_plot(explainer.expected_value[0], shap_values[0]))
    #st_shap(shap.force_plot(explainer.expected_value, shap_values[0, :], X_train.iloc[0, :]))
    #st_shap(shap.force_plot(explainer_tree.expected_value, shap_values_tree, X_train), height=400, width=1000)
-    
 
-with tab3:
-    st.write('Below is a snapshot of the original dataframe')
-
-
-
-
+#def st_shap(plot, height=None):
+#    shap_html = f"<head>{shap.getjs()}</head><body>{plot.html()}</body>"
+#    components.html(shap_html, height=height)
 
 #st_shap(shap.plots.waterfall(shap_values[0]), height=300)
 #st_shap(shap.plots.beeswarm(shap_values), height=300)
 
-#tab1, tab2, tab3 = st.tabs(["Model", "XAI", "3"])
 
-#with tab1:
-#   st.write('Below is a snapshot of the original dataframe')
+
+#st_shap(force_plot = shap.force_plot(explainer_tree.expected_value, shap_values_tree, X_test))
 
--- test_v3.py
+++ test_v3.py
@@ -46,50 +46,6 @@ from sklearn.model_selection import train_test_split
 import matplotlib
 
 
-# Import DataSet from GitHub Respository and define which Excelsheets are to be transferred into pandas dataframes
-path ="https://github.com/KingKai69/VisualAnalytics/raw/main/Automobile_data.csv"
-df = pd.read_csv(path)
-df.head()
-
-# "?"-Symbole durch NaN-Werte ersetzen
-df.replace('?', float('nan'), inplace=True)
-
-# Imputing 
-df_imputed = df.fillna(df.mode().iloc[0])
-
-# Erstellen Sie einen LabelEncoder-Objekt
-le = LabelEncoder()
-
-# Wenden Sie den LabelEncoder auf mehrere Spalten in Ihrem DataFrame an
-columns_to_encode = ['make', 'fuel-type', 'aspiration', 'num-of-doors', 'body-style', 'drive-wheels', 'engine-location','engine-type','num-of-cylinders','fuel-system']  # Liste mit den Spaltennamen
-for column in columns_to_encode:
-    df_imputed[column] = le.fit_transform(df_imputed[column])
-
-# Datentyp der "Alter"-Spalte ändern
-df_imputed['horsepower'] = df_imputed['horsepower'].astype('int64')
-
-# Fixing the SEED
-SEED = 27
-
-# Define features
-features = [
-    'horsepower',
-    'engine-size',
-    'body-style',
-    'num-of-cylinders'
-]
-
-#Define X and Y Data
-x_data = df_imputed[features]
-y_data = df_imputed['highway-mpg']
-
-
-# Modelling
-model = RandomForestRegressor()
-X, X_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.2, random_state=SEED)
-model.fit(X, y_train)
-y_pred = model.predict(X_test)
-
 #######################################################
 ################ MACHINE LEARNING #####################
 #######################################################
@@ -170,22 +126,19 @@ kf_cv = KFold(n_splits = 5, shuffle = True, random_state=SEED)
 
 # Modelling
 clf_final = ExtraTreesClassifier(n_estimators=117, criterion="entropy", max_depth=19 , class_weight='balanced', random_state = SEED)
-A_train, A_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.2, random_state=SEED)
-clf_final.fit(A_train, y_train)
-y_pred = clf_final.predict(A_test)
+X_train, X_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.2, random_state=SEED)
+clf_final.fit(X_train, y_train)
+y_pred = clf_final.predict(X_test)
 
-# compute SHAP values
-#explainer = shap.Explainer(model, X)
-#shap_values = explainer(X)
+##################################################
+################ Compute SHAP ####################
+##################################################
 
-#st_shap(shap.plots.waterfall(shap_values[0]), height=300)
-#st_shap(shap.plots.beeswarm(shap_values), height=300)
+explainer_tree = shap.TreeExplainer(clf_final, X_train)
+shap_values_tree = explainer_tree.shap_values(X_test)
 
-explainer = shap.TreeExplainer(model, X)
-shap_values = explainer.shap_values(X_test)
+##################################################
+################ Dashboard #######################
+##################################################
 
-explainer_2 = shap.TreeExplainer(clf_final, A_train)
-shap_values_2 = explainer.shap_values(A_train)
-
-st_shap(shap.force_plot(explainer.expected_value, shap_values[0,:], X.iloc[0,:]), height=200, width=1000)
-st_shap(shap.force_plot(explainer.expected_value, shap_values, X), height=400, width=1000)
+st_shap(shap.force_plot(explainer_tree.expected_value[0], shap_values_tree[0], X_test))
